f35ba90390dd59debfa5100a292a720f88f8c410
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-07-04 21:37:44 -0300

    adicionando elementos através do jQuery

--- modulo09_jquery/main.js
+++ modulo09_jquery/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
-  console.log(document.querySelector("header button"));
-  console.log($("header button"));
+  $("header button").click(function () {
+    alert("Expandir Formulário");
+  });
+
+  $("form").on("submit", function (e) {
+    console.log("submit");
+    e.preventDefault();
+  });
 });
