1d3a0fea6bc9c70521bb13b3c25b4844724c7a52
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-07-05 16:58:51 -0300

    jQuery

--- modulo09_jquery/main.js
+++ modulo09_jquery/main.js
@@ -8,7 +8,17 @@ $(document).ready(function () {
   });
 
   $("form").on("submit", function (e) {
-    console.log("submit");
     e.preventDefault();
+    const enderecoDaNovaImagem = $("endereco-imagem-nova").val();
+    const novoItem = $("<li></li>");
+    $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem); //criação de um elemento no jQuery, appendTo pega o elemento e joga no Li
+    $(`
+      <div class="overlay-imagem-link">
+        <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+          Ver imagem em tamanho real
+        </a>
+      </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo("ul");
   });
 });
