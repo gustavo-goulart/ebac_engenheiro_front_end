b3486ecf2d63f677527ddbf99e583f5480004324
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-07-06 18:33:24 -0300

    jquery 2

--- modulo09_jquery/exercicio_jquery/src/main.js
+++ modulo09_jquery/exercicio_jquery/src/main.js
@@ -1,12 +1,17 @@
-$(document).ready(function{
-  $("form").on('submit', function(e){
+$(document).ready(function () {
+  $("form").on("submit", function (e) {
     e.preventDefault();
 
-    let novaTarefa = $("#tarefa").val();
-    let novoItem = $("<li style=display:none></li>");
-    $(`<li ${novaTarefa}></li>`).appendTo(novoItem);
-    $(novoItem).appendTo("ul");
-    $("#tarefa").val("");
-  })
-  
+    let novaTarefa = $("input").val();
+    let lista = $("lista");
+    let item = `<li>${tarefa}</li>`;
+    $(item).appendTo(lista);
+    $("button").click(function () {
+      ul.toogle("done");
+    });
+  });
+
+  $("ul").click("li", function () {
+    $(this).addClass("concluido");
+  });
 });
