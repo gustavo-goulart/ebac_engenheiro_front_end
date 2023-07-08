45c43f62901799c5b9ad2227fe2731317231537c
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-07-08 10:47:39 -0300

    jquery validation

--- modulo10_pluguins_jquery/exercicio_plugins_jquery/src/script.js
+++ modulo10_pluguins_jquery/exercicio_plugins_jquery/src/script.js
@@ -10,4 +10,39 @@ $(document).ready(function () {
   $("#cep").mask("00.000-000", {
     placeholder: "00.000-000",
   });
+
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+      },
+      endereco: {
+        required: true,
+      },
+      cpf: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: "Por gentileza digite o nome completo",
+      endereco: "Por gentileza digite o endereço completo",
+    },
+
+    submitHandler: function (form) {
+      console.log(form);
+    },
+
+    invalidHandler: function (evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if (camposIncorretos) {
+        alert(`Existem ${camposIncorretos} campos incorretos`);
+      }
+    },
+  });
 });
